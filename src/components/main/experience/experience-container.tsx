4cd8e78f82fa8a46bd0dca3ac19e61d66799878c
nilloferreiira <danillodana@outlook.com>
2024-03-19 17:53:09 -0300

    add new texts

--- src/components/main/experience/experience-container.tsx
+++ src/components/main/experience/experience-container.tsx
@@ -1,27 +1,34 @@
 export function ExperienceContainer() {
-    return (
-        <div className="w-full flex flex-col lg:flex-row lg:items-center lg:justify-between mx-auto gap-10">
-            <div className="w-full lg:w-96 space-y-5 lg:space-y-10 ">
-              <h2 className="text-zinc-100 font-bold text-3xl lg:text-4xl">Experience</h2>
-              <p className="text-text-secondary text-2xl">
-                3 years working as a Software Developer, in companys such as Itaú,
-                Stone and Aftersale.
-              </p>
-            </div>
+  return (
+    <div className="w-full flex flex-col lg:flex-row lg:items-center lg:justify-between mx-auto gap-10">
+      <div className="w-full lg:w-[588px] space-y-5 lg:space-y-10 ">
+        <h2 className="text-zinc-100 font-bold text-3xl lg:text-4xl">
+          Experience
+        </h2>
+        <p className="text-text-secondary text-2xl">
+          Self-taught in front-end development since 2020, currently focused on
+          development with React and Next.js, alongside building REST
+          APIs with Node.js.
+        </p>
+      </div>
 
-            <div className="w-full lg:w-96 space-y-5 lg:space-y-10 ">
-              <h2 className="text-zinc-100 font-bold text-3xl lg:text-4xl">Education</h2>
-              <p className="text-text-secondary text-2xl ">
-                Computer Science Bachelors Degree - Universidade Federal de Pelotas
-                🎓
-              </p>
-            </div>
+      <div className="w-full lg:w-96 space-y-5 lg:space-y-10 ">
+        <h2 className="text-zinc-100 font-bold text-3xl lg:text-4xl">
+          Education
+        </h2>
+        <p className="text-text-secondary text-2xl ">
+          Associate Degree in Systems Analysis and Development - Universidade
+          Tiradentes 🎓
+        </p>
+      </div>
 
-            <div className="w-full lg:w-96 space-y-5 lg:space-y-10 ">
-              <h2 className="text-zinc-100 font-bold text-3xl lg:text-4xl">Languages</h2>
-              <p className="text-text-secondary text-2xl">🇺🇸 english</p>
-              <p className="text-text-secondary text-2xl">🇧🇷 portugues</p>
-            </div>
-        </div>
-    )
+      <div className="w-full lg:w-96 space-y-5 lg:space-y-10 ">
+        <h2 className="text-zinc-100 font-bold text-3xl lg:text-4xl">
+          Languages
+        </h2>
+        <p className="text-text-secondary text-2xl">🇺🇸 english</p>
+        <p className="text-text-secondary text-2xl">🇧🇷 portugues</p>
+      </div>
+    </div>
+  );
 }
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,27 +6,30 @@ export function getProjects() {
       project: {
         id: 1,
         title: "Read.ED",
-        description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        imgSrc: "https://github.com/nilloferreiira/readed/raw/main/src/assets/readmeimg.png",
+        description: "Your personal online library!",
+        imgSrc:
+          "https://github.com/nilloferreiira/readed/raw/main/src/assets/readmeimg.png",
         url: "https://readed.vercel.app/",
       },
     },
     {
       project: {
         id: 2,
-        title: "Types Quotes",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        imgSrc: "https://github.com/nilloferreiira/type-quotes/raw/main/public/assets/readmeimg.png",
+        title: "Type Quotes",
+        description:
+          "Enhance your typing skills while diving into philosophy with the quotes from this app!",
+        imgSrc:
+          "https://github.com/nilloferreiira/type-quotes/raw/main/public/assets/readmeimg.png",
         url: "https://type-quotes.vercel.app/",
       },
     },
     {
       project: {
         id: 3,
-        title: "Ai movie",
-        description: "Ai movie is an application blablalbalbla",
-        imgSrc: "https://github.com/nilloferreiira/ai-movie/blob/main/public/assets/readmeimg.png?raw=true",
+        title: "AI movie",
+        description: "Choose your next movie using AI",
+        imgSrc:
+          "https://github.com/nilloferreiira/ai-movie/blob/main/public/assets/readmeimg.png?raw=true",
         url: "https://ai-movie-nilloferreiira.vercel.app/",
       },
     },
@@ -34,17 +37,19 @@ export function getProjects() {
       project: {
         id: 4,
         title: "Expert Notes",
-        description: "Expert notes is an application blablalbalbla",
-        imgSrc: "https://github.com/nilloferreiira/NLW-Expert/raw/master/public/readmeimg.png",
+        description: "An notes application with React",
+        imgSrc:
+          "https://github.com/nilloferreiira/NLW-Expert/raw/master/public/readmeimg.png",
         url: "https://expert-notes-nilloferreiira.vercel.app/",
       },
     },
     {
       project: {
         id: 5,
-        title: "Spotify ui cline",
-        description: "Spotify ui clone is an application blablalbalbla",
-        imgSrc: "https://github.com/nilloferreiira/Spotify-clone/raw/main/public/assets/readmeimg.png",
+        title: "Spotify UI clone",
+        description: "Spotify ui clone",
+        imgSrc:
+          "https://github.com/nilloferreiira/Spotify-clone/raw/main/public/assets/readmeimg.png",
         url: "https://spotify-clone-opal-ten.vercel.app/",
       },
     },
@@ -52,5 +57,3 @@ export function getProjects() {
 
   return { projects };
 }
-
-
