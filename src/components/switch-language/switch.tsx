cf9f11ffbbb23e9069778eba1f1acca648f4d673
nilloferreiira <danillodana@outlook.com>
2024-03-19 22:20:48 -0300

    label for the switch

--- src/components/switch-language/switch.tsx
+++ src/components/switch-language/switch.tsx
@@ -5,21 +5,24 @@ interface SwitchProps {
 
 export function SwitchLanguage({ value, toggleLanguage }: SwitchProps) {
   return (
-    <div
-      onClick={toggleLanguage}
-      className={`md:w-14 md:h-7 w-12 h-6 flex items-center ${
-        value === true ? "bg-zinc-900" : "bg-gray-300"
-      } rounded-full p-1 cursor-pointer`}
-    >
-      {/* switch options */}
+    <div>
       <div
-        className={`
-            ${value === true ? "bg-gray-300" : "bg-zinc-900"}
-            md:w-6 md:h-6 w-5 h-5 rounded-full shadow-md transition duration-500 transform ${
-              value === true ? "translate-x-6" : null
-            }
-            `}
-      />
+        onClick={toggleLanguage}
+        className={`md:w-14 md:h-7 w-12 h-6 flex items-center ${
+          value === true ? "bg-zinc-900" : "bg-gray-300"
+        } rounded-full p-1 cursor-pointer`}
+      >
+        {/* switch options */}
+        <div
+          className={`
+              ${value === true ? "bg-gray-300" : "bg-zinc-900"}
+              md:w-6 md:h-6 w-5 h-5 rounded-full shadow-md transition duration-500 transform ${
+                value === true ? "translate-x-6" : null
+              }
+              `}
+        />
+      </div>
+    <span className="text-text-secondary text-sm">BR - US</span>
     </div>
   );
 }
