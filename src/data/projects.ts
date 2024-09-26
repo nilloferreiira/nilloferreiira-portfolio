44277f45e3f032ed02c186b86500ca58dbfb78e7
Danillo <danillodana@outlook.com>
2024-09-26 10:53:36 -0300

    add in.orbit and legacy homes

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,4 +1,4 @@
-import { ProjectProps } from "../components/main/projects/project-card";
+import { ProjectProps } from "../components/main/projects/project-card"
 
 export function getProjects() {
   const projects: ProjectProps[] = [
@@ -10,8 +10,8 @@ export function getProjects() {
         descriptionUS: "Your personal online library!",
         imgSrc:
           "https://github.com/nilloferreiira/readed/raw/main/src/assets/readmeimg.png",
-        url: "https://readed.vercel.app/",
-      },
+        url: "https://readed.vercel.app/"
+      }
     },
     {
       project: {
@@ -24,8 +24,8 @@ export function getProjects() {
 
         imgSrc:
           "https://github.com/nilloferreiira/type-quotes/raw/main/public/assets/readmeimg.png",
-        url: "https://type-quotes.vercel.app/",
-      },
+        url: "https://type-quotes.vercel.app/"
+      }
     },
     {
       project: {
@@ -35,32 +35,54 @@ export function getProjects() {
         descriptionUS: "Choose your next movie using AI",
         imgSrc:
           "https://github.com/nilloferreiira/ai-movie/blob/main/public/assets/readmeimg.png?raw=true",
-        url: "https://ai-movie-nilloferreiira.vercel.app/",
-      },
+        url: "https://ai-movie-nilloferreiira.vercel.app/"
+      }
     },
     {
       project: {
         id: 4,
+        title: "In.Orbit",
+        descriptionBR: "Um Aplicativo de gerenciamento de metas.",
+        descriptionUS: "A goal management application.",
+        imgSrc:
+          "https://github.com/nilloferreiira/InOrbit-web/raw/main/public/assets/readmeimg.png",
+        url: "https://github.com/nilloferreiira/InOrbit-web"
+      }
+    },
+    {
+      project: {
+        id: 5,
         title: "Expert Notes",
         descriptionBR: "Um aplicativo de notas feito em React",
         descriptionUS: "An notes application with React",
         imgSrc:
           "https://github.com/nilloferreiira/NLW-Expert/raw/master/public/readmeimg.png",
-        url: "https://expert-notes-nilloferreiira.vercel.app/",
-      },
+        url: "https://expert-notes-nilloferreiira.vercel.app/"
+      }
     },
     {
       project: {
-        id: 5,
+        id: 6,
+        title: "Legacy Homes",
+        descriptionBR: "Landing page moderna com animações.",
+        descriptionUS: "Modern landing page with animations.",
+        imgSrc:
+          "https://github.com/nilloferreiira/Legacy-Homes/raw/main/public/assets/readmeimg.png",
+        url: "https://github.com/nilloferreiira/Legacy-Homes"
+      }
+    },
+    {
+      project: {
+        id: 7,
         title: "Spotify UI clone",
         descriptionBR: "Clone da interface visual do Spotify",
         descriptionUS: "Spotify ui clone",
         imgSrc:
           "https://github.com/nilloferreiira/Spotify-clone/raw/main/public/assets/readmeimg.png",
-        url: "https://spotify-clone-opal-ten.vercel.app/",
-      },
-    },
-  ];
+        url: "https://spotify-clone-opal-ten.vercel.app/"
+      }
+    }
+  ]
 
-  return { projects };
+  return { projects }
 }
